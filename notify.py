b9c4d0edff3711386183d9fb20e56f5a875812b2
Mateusz <mkuliberda@gmail.com>
2019-09-25 20:01:58 +0200

    remote ssh editing and src control check

--- notify.py
+++ notify.py
@@ -12,13 +12,6 @@ from email_handler import Class_eMail
 To_Email_ID = "mkuliberda@gmail.com"
 DETECTIONS_PATH = '/home/pi/Desktop/Detections/'
 
-
-# Send Plain Text Email 
-#email = Class_eMail()
-#email.send_Text_Mail(To_Email_ID, 'Plain Text Mail Subject', 'This is sample plain test email body.')
-#del email
-
-
 # Send HTML Email
 email = Class_eMail()
 #email.send_HTML_Mail(To_Email_ID, 'Home Monitoring', '<html><h1>Someone is in the house</h1></html>')
